fc7cf3768a98788573e1ad3d1109781b2a4d36bc
Diana Forostiana <dianaforost@gmail.com>
2023-02-16 14:53:29 +0100

    Update 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
-
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+btnStart.style.padding = '10px';
+btnStop.style.padding = '10px';
+let timerId;
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.setAttribute('disabled', true);
+});
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.removeAttribute('disabled');
+});
